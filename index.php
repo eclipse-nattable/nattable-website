1df62eb9f31afc8524afdc2caa2d81835898b9b3
Dirk Fauth <dirk.fauth@googlemail.com>
2014-06-13 22:50:35 +0200

    Fixed several layout issues with solstice theme
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- index.php
+++ index.php
@@ -27,6 +27,6 @@
 	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>');
 	
 	# Generate the web page
-	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 
 ?>
